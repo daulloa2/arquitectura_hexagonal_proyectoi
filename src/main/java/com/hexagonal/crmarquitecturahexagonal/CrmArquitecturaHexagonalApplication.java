dcca2986882cb8dc9f7291d509d0a51a95d3922b
daulloa2 <daulloa2@utpl.edu.ec>
2022-01-12 06:35:43 -0500

    Creando proyecto de inicializacion

---
+++ src/main/java/com/hexagonal/crmarquitecturahexagonal/CrmArquitecturaHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.hexagonal.crmarquitecturahexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmArquitecturaHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmArquitecturaHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexagonal/crmarquitecturahexagonal/CrmArquitecturaHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexagonal.crmarquitecturahexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmArquitecturaHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
